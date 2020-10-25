bdb64d2336f7f0fd5bf34f5fb6a7dcd5827694b7
Jeffery Adusei <faculty294@gmail.com>
2020-10-25 09:40:18 +0000

    Created new class to handle checking connectivity of a web HTTP/HTTPS server

---
+++ Source/Cored/Web/ConnectivityChecker.cs
@@ -0,0 +1,15 @@
+﻿namespace Cored.Web
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Continually hits  up a web HTTP/HTTPS endpoint at a specified interval, checking for a valid response
+    /// This is particularly helpful for checking the connectivity of a website.
+    /// </summary>
+    public class ConnectivityChecker
+    {
+        
+    }
+}
