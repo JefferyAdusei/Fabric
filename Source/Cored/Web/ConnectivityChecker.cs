9fb446f6d7ec9d2410d4558b6f1f58d43e24b4d5
Jeffery Adusei <faculty294@gmail.com>
2020-11-03 01:44:00 +0000

    Added the ability to hit up a network continually at a specified interval to check for connectivity.

--- Source/Cored/Web/ConnectivityChecker.cs
+++ Source/Cored/Web/ConnectivityChecker.cs
@@ -8,7 +8,7 @@
     /// Continually hits  up a web HTTP/HTTPS endpoint at a specified interval, checking for a valid response
     /// This is particularly helpful for checking the connectivity of a website.
     /// </summary>
-    public class ConnectivityChecker
+    public class ConnectivityChecker : IDisposable
     {
         #region Private Members
 
@@ -46,7 +46,7 @@
 
         #region Connectivity Methods
 
-        public async Task<bool> PingAsync(string endpoint, Func<HttpWebResponse, bool> validResponseParser)
+        public async Task<bool> PingAsync(string endpoint, Func<HttpWebResponse, bool> validResponseParser = null)
         {
             HttpWebResponse webResponse = await WebRequests.GetAsync(endpoint);
 
@@ -55,6 +55,67 @@
             webResponse?.Close();
 
             return responsive;
+        }
+
+        /// <summary>
+        /// Hits up a web HTTP/HTTPS endpoint continually at a given interval to determine whether the server
+        /// is responsive.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to do a GET call on</param>
+        /// <param name="interval">The time between periodical checks in milliseconds</param>
+        /// <param name="stateChangedCallback">Fired when the endpoint state changes (responsive/not responsive)</param>
+        /// <param name="validResponseParser">
+        ///     If specified, handles whether the <see cref="HttpWebResponse"/> from the server should be classified
+        ///     as successful or not.
+        /// </param>
+        /// <returns></returns>
+        public async Task LinearRetryAsync(string endpoint, TimeSpan interval, Action<bool> stateChangedCallback,
+            Func<HttpWebResponse, bool> validResponseParser = null)
+        {
+            while (!_disposing)
+            {
+                /*
+                 * Assume that any response from the server is valid even it is a
+                 * 401. This is because a page not found or server error actually
+                 * is a server response.
+                 */
+                HttpWebResponse webResponse = await WebRequests.GetAsync(endpoint);
+
+                /*
+                 * If there is a valid response parser, ask it for the state based on the response
+                 * otherwise, so long as there is a response of any kind, it's valid.
+                 */
+                bool responsive = validResponseParser?.Invoke(webResponse) ?? webResponse != null;
+
+                /*
+                 * If the server is returns a value different from the value of the responsive
+                 * property, then set the responsive property to the server response.
+                 * This is to make sure that we only invoke the stateChangedCallback only when
+                 * there is a change in the server response
+                 */
+                if (responsive != Responsive)
+                {
+                    Responsive = responsive;
+
+                    // Inform the action about the change in the network/connectivity
+                    stateChangedCallback?.Invoke(responsive);
+                }
+                // Close the web response
+                webResponse?.Close();
+
+                // Delay for the given timespan before starting over the loop.
+                await Task.Delay(interval);
+            }
+        }
+
+        #endregion
+
+        #region Implementation of IDisposable
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _disposing = true;
         }
 
         #endregion
