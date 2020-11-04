24249edf1cedc54d72820be959be84291b1c4459
Jeffery Adusei <faculty294@gmail.com>
2020-11-04 04:17:02 +0000

    Added ability to check for connectivity of an endpoint in an exponential interval

--- Source/Cored/Web/ConnectivityChecker.cs
+++ Source/Cored/Web/ConnectivityChecker.cs
@@ -1,4 +1,6 @@
-﻿namespace Cored.Web
+﻿using System.Xml.Schema;
+
+namespace Cored.Web
 {
     using System;
     using System.Net;
@@ -108,6 +110,65 @@
             }
         }
 
+/// <summary>
+        /// Hits up a web HTTP/HTTPS endpoint continually at a given interval to determine whether the server
+        /// is responsive.
+        /// </summary>
+        /// <param name="endpoint">The endpoint to do a GET call on</param>
+        /// <param name="interval">The time between periodical checks in milliseconds</param>
+        /// <param name="stateChangedCallback">Fired when the endpoint state changes (responsive/not responsive)</param>
+        /// <param name="validResponseParser">
+        ///     If specified, handles whether the <see cref="HttpWebResponse"/> from the server should be classified
+        ///     as successful or not.
+        /// </param>
+        /// <returns></returns>
+        public async Task ExponentialRetryAsync(string endpoint, TimeSpan interval, Action<bool> stateChangedCallback,
+            Func<HttpWebResponse, bool> validResponseParser = null)
+        {
+            int exponent = 0;
+
+            while (!_disposing)
+            {
+                /*
+                 * Assume that any response from the server is valid even it is a
+                 * 401. This is because a page not found or server error actually
+                 * is a server response.
+                 */
+                HttpWebResponse webResponse = await WebRequests.GetAsync(endpoint);
+
+                /*
+                 * If there is a valid response parser, ask it for the state based on the response
+                 * otherwise, so long as there is a response of any kind, it's valid.
+                 */
+                bool responsive = validResponseParser?.Invoke(webResponse) ?? webResponse != null;
+
+                /*
+                 * If the server is returns a value different from the value of the responsive
+                 * property, then set the responsive property to the server response.
+                 * This is to make sure that we only invoke the stateChangedCallback only when
+                 * there is a change in the server response
+                 */
+                if (responsive != Responsive)
+                {
+                    Responsive = responsive;
+
+                    // Inform the action about the change in the network/connectivity
+                    stateChangedCallback?.Invoke(responsive);
+                }
+                // Close the web response
+                webResponse?.Close();
+
+                /*
+                 * For every new attempt, raise the interval by an exponential value.
+                 * This is to ensure that the frequency of the web request does not overwhelm the server.
+                 * Because for every new request the time to retry is slightly higher than the previous.
+                 */
+                interval = TimeSpan.FromMilliseconds(interval.Milliseconds * Math.Pow(2, exponent++));
+
+                // Delay for the given timespan before starting over the loop.
+                await Task.Delay(interval);
+            }
+        }
         #endregion
 
         #region Implementation of IDisposable
