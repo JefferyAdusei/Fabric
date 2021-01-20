33a456eade424adb4c531449b792314b4dfd15bf
Jeffery Adusei <faculty294@gmail.com>
2021-01-20 11:59:24 +0000

    Added file path extension method to normalize the path of a file bases on the operating system the application is running on

---
+++ Source/Cored/Logging/PathExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Cored.Logging
+{
+    using Fabric.Di;
+    using System.IO;
+
+    /// <summary>
+    /// Extension method that normalizes file path based on the current OS Platform
+    /// </summary>
+    public static class PathExtensions
+    {
+        /// <summary>
+        /// Normalizes a path based on the current operating system
+        /// </summary>
+        /// <param name="path">The path to normalize</param>
+        /// <returns>The normalized path</returns>
+        public static string NormalizePath(this string path) => FabricDi.FabricEnvironment.IsWindows
+            ? path?.Replace('/', '\\').Trim()
+            : path?.Replace('\\', '/').Trim();
+
+        /// <summary>
+        /// Resolves any relative elements of the path to absolute
+        /// </summary>
+        /// <param name="path">The path to resolve</param>
+        /// <returns>The resolved path</returns>
+        public static string ResolvePath(this string path) => Path.GetFullPath(path);
+    }
+}
